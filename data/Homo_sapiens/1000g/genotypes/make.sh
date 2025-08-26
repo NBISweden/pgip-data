b0703f64f83d6935bd6e9e9c8a39b18a9eb53497
Per Unneberg <per.unneberg@scilifelab.se>
2025-08-26 22:34:50 +0200

    Setup pixi support

--- data/Homo_sapiens/1000g/genotypes/make.sh
+++ data/Homo_sapiens/1000g/genotypes/make.sh
@@ -6,16 +6,16 @@ wget https://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/integrated_call
 
 # Make population-specific sample files
 panel=integrated_call_samples_v3.20130502.ALL.panel
-awk '{if ($2 == "CEU") print $1}' $panel > CEU.samples.txt
-awk '{if ($2 == "CHB") print $1}' $panel > CHB.samples.txt
-awk '{if ($2 == "YRI") print $1}' $panel > YRI.samples.txt
+awk '{if ($2 == "CEU") print $1}' $panel >CEU.samples.txt
+awk '{if ($2 == "CHB") print $1}' $panel >CHB.samples.txt
+awk '{if ($2 == "YRI") print $1}' $panel >YRI.samples.txt
 
 # Subset vcf to these files
 VCF=ALL.chr22.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.vcf.gz
 REGION=22:17000000-20000000
 for pop in CEU CHB YRI; do
     echo $pop
-    bcftools view -v snps -m 2 -M 2 -S $pop.samples.txt $VCF $REGION | bgzip -c > $pop.$REGION.vcf.gz
+    bcftools view -v snps -m 2 -M 2 -S $pop.samples.txt $VCF $REGION | bgzip -c >$pop.$REGION.vcf.gz
     tabix -f $pop.$REGION.vcf.gz
     plink --vcf $pop.$REGION.vcf.gz --hardy --out $pop.$REGION
     gzip -v $pop.${REGION}.hwe
