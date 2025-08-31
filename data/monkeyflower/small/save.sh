8ced16e41854c0bb6f7a5610ff7e09dbd533afeb
Per Unneberg <per.unneberg@scilifelab.se>
2025-08-31 16:41:36 +0200

    Reorganize monkeyflower result files

---
+++ data/monkeyflower/small/save.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+INPUT_DIRECTORY="unset"
+
+usage() {
+    echo "Usage: $0 [-d input_directory] rsync_options"
+    exit 2
+}
+
+while getopts "d:h" opt; do
+    case $opt in
+    h)
+        usage
+        ;;
+    d)
+        INPUT_DIRECTORY=$OPTARG
+        shift $((OPTIND - 1))
+        ;;
+    \?)
+        echo "Invalid option: -$OPTARG" >&2
+        usage
+        ;;
+    esac
+done
+
+if [ "$INPUT_DIRECTORY" = "unset" ]; then
+    echo "Error: input directory not set"
+    usage
+fi
+
+OPTIONS=("$@")
+
+echo "${OPTIONS[@]}"
+echo running rsync -amv "${OPTIONS[@]}" "${INPUT_DIRECTORY}" ...
+
+rsync -amv "${OPTIONS[@]}" \
+    --include="*/" \
+    --include="*.variantsites.vcf.gz" \
+    --include="*.variantsites.vcf.gz.tbi" \
+    --exclude="*" \
+    "${INPUT_DIRECTORY}/gatk-genotype-gvcf-bqsr/" \
+    .
---
+++ data/monkeyflower/tiny/save.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+INPUT_DIRECTORY="unset"
+
+usage() {
+    echo "Usage: $0 [-d input_directory] rsync_options"
+    exit 2
+}
+
+while getopts "d:h" opt; do
+    case $opt in
+    h)
+        usage
+        ;;
+    d)
+        INPUT_DIRECTORY=$OPTARG
+        shift $((OPTIND - 1))
+        ;;
+    \?)
+        echo "Invalid option: -$OPTARG" >&2
+        usage
+        ;;
+    esac
+done
+
+if [ "$INPUT_DIRECTORY" = "unset" ]; then
+    echo "Error: input directory not set"
+    usage
+fi
+
+OPTIONS=("$@")
+
+echo "${OPTIONS[@]}"
+echo running rsync -amv "${OPTIONS[@]}" "${INPUT_DIRECTORY}" ...
+
+rsync -amv "${OPTIONS[@]}" \
+    --include="*/" \
+    --exclude="*.subset.vcf.gz*" \
+    --exclude="*.vcf.gz.bcftools.stats" \
+    --exclude="*" \
+    "${INPUT_DIRECTORY}/gatk-hc-bqsr" \
+    "${INPUT_DIRECTORY}/ubam" \
+    "${INPUT_DIRECTORY}/fastq" \
+    "${INPUT_DIRECTORY}/ref" \
+    "${INPUT_DIRECTORY}/rm" \
+    "${INPUT_DIRECTORY}/gatk-genotype-gvcf-bqsr" \
+    "${INPUT_DIRECTORY}/gatk-combine-gvcf-bqsr" \
+    .
